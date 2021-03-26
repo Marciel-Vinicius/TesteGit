7b5919c2c1b086c1848de6565f142e9e41ec1e31
Marciel-Vinicius <marcielviniciusdelara@gmail.com>
2021-03-25 22:41:12 -0300

    PrimeiroProjeto

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		int y = 32;
+		double x = 3.7465758;
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println("Bom dia!");
+
+	}
+
+}
